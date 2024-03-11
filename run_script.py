d655db82bb7dac9535b1812aea3beefa717a9af6
Jeniffer Hurtado Coronado <62664863+Jennimar17@users.noreply.github.com>
2024-03-11 15:35:10 -0300

    initial

---
+++ run_script.py
@@ -0,0 +1,16 @@
+# Contributions-Importer-For-Github/run_script.py
+import git
+from git_contributions_importer import *
+
+# Your private repo or Bitbucket repo
+repo = git.Repo(r"C:\Users\jenni\Documents\UALABEE\CMS\site-ualabee-cms-3")
+# Your mock repo
+mock_repo = git.Repo(r"C:\Users\jenni\Documents\contributions\Contributions-Importer-For-Github")
+importer = Importer([repo], mock_repo)
+# I use both my personal email and work email here,
+# Since the private repo uses work email, and Github uses my personal email
+importer.set_author(['jennimar17@gmail.com', 'jeniffer@ualabee.com', '62664863+Jennimar17@users.noreply.github.com'])
+importer.import_repository()
+# pip3 install gitpython
+# pip3 install pathlib 
+# run ✗ python3 run_script.py
